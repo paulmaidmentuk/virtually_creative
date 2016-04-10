16126c6d5a9945624592852f83fbe6410db98e76
Paul Maidment <paul.maidment@affiliatewindow.com>
2016-04-10 02:22:59 +0100

    Twitter API integration

--- MainLobby/Assets/Scripts/Audio.cs
+++ MainLobby/Assets/Scripts/Audio.cs
@@ -1,19 +1,33 @@
 ﻿using UnityEngine;
 using SimpleJSON;
 using System.Collections;
+using System.IO;
 
 public class Audio : MonoBehaviour {
 
-    public AudioSource audio;
+    public AudioSource audio1;
+    public AudioSource audio2;
+    public TextMesh textMesh;
+    public Renderer renderer1;
     private bool isPlaying = true;
     private JSONNode jsonData;
 
     // Use this for initialization
     IEnumerator Start () {
+        textMesh.text = "Introducing Spotify API";
+        yield return new WaitForSeconds(2);
         WWW www = new WWW("http://localhost/index.php");
         yield return www;
         jsonData = JSON.Parse(www.text);
+        textMesh.text = "Artist: " + jsonData["artists"][0]["name"].Value + " \n" + jsonData["name"].Value;
+
+        audio1.Play();
+        WWW www2 = new WWW("http://localhost/index2.php");
+        yield return www2;
+        jsonData = JSON.Parse(www2.text);
     }
+
+
 
     // Update is called once per frame
     void Update()
@@ -22,23 +36,29 @@ public class Audio : MonoBehaviour {
         {
             if (isPlaying)
             {
-                audio.Pause();
+                audio2.Pause();
                 isPlaying = false;
+                textMesh.text = "Playing paused";
             }
             else
             {
-                audio.Play();
+                audio2.Play();
                 isPlaying = true;
+                textMesh.text = "Artist: " + jsonData["artists"][0]["name"].Value + " \n" + jsonData["name"].Value;
             }
         }
         if (Input.GetKeyDown(KeyCode.M))
         {
             Application.OpenURL("http://spotify.com/");
         }
+
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            textMesh.text = "Looking for next song";
+            audio1.Stop();
+            audio2.Play();
+            textMesh.text = "Artist: " + jsonData["artists"][0]["name"].Value + " \n" + jsonData["name"].Value;
+        }
     }
 
-    void OnMouseOver()
-    {
-        Debug.Log(jsonData["artists"][0]["name"].Value + " " + jsonData["name"].Value);
-    }
 }
